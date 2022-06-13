77663685bf342e96e287030c6b8e745b1c0c02dc
karna <karnagogoi7@gmail.com>
2022-06-13 23:38:15 +0530

    Add SiteViews chart

---
+++ src/views/SiteViewsChart.vue
@@ -0,0 +1,29 @@
+<template>
+  <Chart
+    title="Site Views"
+    chartId="lineChart"
+    type="line"
+    :categories="timeline"
+    :chartData="siteViews"
+    dataLabel="Site Views"
+    xlabel="Time (24hrs)"
+    ylabel="Number of Views"
+  />
+</template>
+<script>
+import { timeline, siteViews } from "@/util/chartData";
+import Chart from "@/components/Chart/Chart.vue";
+
+export default {
+  name: "SiteViewChart",
+  components: {
+    Chart,
+  },
+  data() {
+    return {
+      timeline,
+      siteViews,
+    };
+  },
+};
+</script>
