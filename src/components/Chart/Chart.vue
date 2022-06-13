7cdcba739daca2294d52310c4f757120f569ca0d
karna <karnagogoi7@gmail.com>
2022-06-13 22:38:51 +0530

    Update chart component
    Add label for dataset in chart
    Make chart reactive to chart data

--- src/components/Chart/Chart.vue
+++ src/components/Chart/Chart.vue
@@ -7,12 +7,15 @@
 </template>
 <script>
 import { generateChart } from "./util";
+import { isEqual } from "lodash";
+
 export default {
   name: "Chart",
   props: {
     chartId: String,
     categories: Array,
     chartData: Array,
+    dataLabel: String,
     type: String,
     xlabel: String,
     ylabel: String,
@@ -22,18 +25,40 @@ export default {
       chart: null,
     };
   },
+  watch: {
+    chartConfig(newConfig, oldConfig) {
+      if (!isEqual(newConfig, oldConfig)) {
+        this.renderChart();
+      }
+    },
+  },
+  computed: {
+    chartConfig() {
+      const type = this.type || "bar";
+      const catagories = this.catagories || [];
+      const chartData = this.chartData || [];
+      const dataLabel = this.dataLabel || "Data";
+
+      return {
+        type,
+        catagories,
+        chartData: [dataLabel, ...chartData],
+        xlabel: this.xlabel,
+        ylabel: this.ylabel,
+      };
+    },
+  },
   mounted() {
     this.chart = this.renderChart();
   },
   methods: {
     renderChart() {
+      const chartConfig = this.chartConfig;
+      const target = this.$refs.chartContainer;
+
       return generateChart({
-        target: this.$refs.chartContainer,
-        type: this.type,
-        catagories: this.categories,
-        chartData: this.chartData,
-        xlabel: this.xlabel,
-        ylabel: this.ylabel,
+        target,
+        ...chartConfig,
       });
     },
   },
