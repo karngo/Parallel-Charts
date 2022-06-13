9220dbbd6280d90a24434d9f74277354e45517f5
karna <karnagogoi7@gmail.com>
2022-06-13 23:33:01 +0530

    Add PageViews chart

---
+++ src/views/PageViewChart.vue
@@ -0,0 +1,29 @@
+<template>
+  <Chart
+    title="Page Views"
+    chartId="barChart"
+    type="bar"
+    :categories="pageTitles"
+    :chartData="pageVisits"
+    dataLabel="2022-01-01"
+    xlabel="Page Titles"
+    ylabel="Number of Views"
+  />
+</template>
+<script>
+import { pageTitles, pageVisits } from "@/util/chartData";
+import Chart from "@/components/Chart/Chart.vue";
+
+export default {
+  name: "PageViewChart",
+  components: {
+    Chart,
+  },
+  data() {
+    return {
+      pageTitles,
+      pageVisits,
+    };
+  },
+};
+</script>
