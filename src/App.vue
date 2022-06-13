ecf9fe844f5d550f645be481816b1de3da66fc60
karna <karnagogoi7@gmail.com>
2022-06-13 23:20:30 +0530

    Add vueitfy root container
    For applying vuetify styles

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,41 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col>
-        <h2>Parallel Charts</h2>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <Chart
-          chartId="barChart"
-          type="bar"
-          :categories="pageTitles"
-          :chartData="pageVisits"
-          dataLabel="2022-01-01"
-          xlabel="Page Titles"
-          ylabel="Number of Views"
-        />
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <Chart
-          chartId="lineChart"
-          type="line"
-          :categories="timeline"
-          :chartData="siteViews"
-          dataLabel="Site Views"
-          xlabel="Time (24hrs)"
-          ylabel="Number of Views"
-        />
-      </v-col>
-    </v-row>
-  </v-container>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col>
+            <h2>Parallel Charts</h2>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <Chart
+              chartId="barChart"
+              type="bar"
+              :categories="pageTitles"
+              :chartData="pageVisits"
+              dataLabel="2022-01-01"
+              xlabel="Page Titles"
+              ylabel="Number of Views"
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col>
+            <Chart
+              chartId="lineChart"
+              type="line"
+              :categories="timeline"
+              :chartData="siteViews"
+              dataLabel="Site Views"
+              xlabel="Time (24hrs)"
+              ylabel="Number of Views"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 <script>
 import { pageTitles, pageVisits, timeline, siteViews } from "./util/chartData";
